628ea238538d98e8ea7dceacdbcd75dd49f5d425
Daniel M Sanchez <dsanchez@existaya.com>
2018-09-15 11:56:51 -0500

    se agrego variable que me controla el cargador en el home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,12 +9,15 @@ import { SpotifyService } from '../../services/spotify.service';
 })
 export class HomeComponent {
   newSongs: any[] = [];
+  loading: boolean;
 
   constructor( private _spotifyService: SpotifyService ) {
+    this.loading = true;
     this._spotifyService.getNewReleases()
         .subscribe( (data: any) => {
           console.log(data);
           this.newSongs = data;
+          this.loading = false;
         });
   }
 
