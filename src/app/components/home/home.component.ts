70802a2ad355260768d462effc2956cddeaedb06
Daniel M Sanchez <dsanchez@existaya.com>
2018-09-15 08:32:45 -0500

    se puso logica en el componente del home que me trae las nuevas canciones y son mostradas en el html del componente

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styles: []
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  newSongs: any[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor( private _spotifyService: SpotifyService ) {
+    this._spotifyService.getNewReleases()
+        .subscribe( (data: any) => {
+          console.log(data);
+          this.newSongs = data.albums.items;
+        });
   }
 
 }
