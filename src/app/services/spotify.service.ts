2cc9a208bfef729202d2ebf7867d0f9bdc583b99
Daniel M Sanchez <dsanchez@existaya.com>
2018-09-15 09:11:37 -0500

    se parametrizaron variables

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -6,23 +6,23 @@ import { map } from 'rxjs/operators';
   providedIn: 'root'
 })
 export class SpotifyService {
+  apiUrl: any = 'https://api.spotify.com/v1/';
   token: any = 'BQARxUWgO-IdR99sDTPjJGGAwOtkj5LyapdMuK0PnSkoEbm585CgO81SG4xhYGP9u64igAsqsGc9WrdXapUYJkdiM0L5WqsoKBAmvNlHxDjO_YA0Q38V1nY4lcx6m_P_Ht9SyQhK2_kjuS8';
+  headers: any = {
+      'Authorization': `Bearer ${this.token}`
+  };
 
   constructor(private _http: HttpClient) { }
 
   getNewReleases() {
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.token}`
-    });
+    const headers = new HttpHeaders(this.headers);
 
-    return this._http.get('https://api.spotify.com/v1/browse/new-releases', { headers }).pipe( map( data => data['albums'].items ));
+    return this._http.get(`${this.apiUrl}browse/new-releases`, { headers }).pipe( map( data => data['albums'].items ));
   }
 
   getArtist(term: string) {
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.token}`
-    });
+    const headers = new HttpHeaders(this.headers);
 
-    return this._http.get(`https://api.spotify.com/v1/search?q=${term}&type=artist&limit=15`, { headers }).pipe( map( data => data['artists'].items ));
+    return this._http.get(`${this.apiUrl}search?q=${term}&type=artist&limit=15`, { headers }).pipe( map( data => data['artists'].items ));
   }
 }
