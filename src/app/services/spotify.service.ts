5b2815aaad615ac7da0c39b5176c323d970d678f
Daniel M Sanchez <dsanchez@existaya.com>
2018-09-15 08:54:55 -0500

    se separo el token en variable y se creo metodo nuevo que me obtiene los artistas por nombre

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -5,14 +5,23 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
   providedIn: 'root'
 })
 export class SpotifyService {
+  token: any = 'BQARxUWgO-IdR99sDTPjJGGAwOtkj5LyapdMuK0PnSkoEbm585CgO81SG4xhYGP9u64igAsqsGc9WrdXapUYJkdiM0L5WqsoKBAmvNlHxDjO_YA0Q38V1nY4lcx6m_P_Ht9SyQhK2_kjuS8';
 
   constructor(private _http: HttpClient) { }
 
   getNewReleases() {
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQARxUWgO-IdR99sDTPjJGGAwOtkj5LyapdMuK0PnSkoEbm585CgO81SG4xhYGP9u64igAsqsGc9WrdXapUYJkdiM0L5WqsoKBAmvNlHxDjO_YA0Q38V1nY4lcx6m_P_Ht9SyQhK2_kjuS8'
+      'Authorization': `Bearer ${this.token}`
     });
 
     return this._http.get('https://api.spotify.com/v1/browse/new-releases', { headers });
+  }
+
+  getArtist(term: string) {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.token}`
+    });
+
+    return this._http.get(`https://api.spotify.com/v1/search?q=${term}&type=artist&limit=15`, { headers });
   }
 }
